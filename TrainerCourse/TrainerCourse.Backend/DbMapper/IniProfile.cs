298dd25a05000503e029134ca977610dff3471b2
Yeudanta Mahardika Aditya Wicaksana <159878204+yuudantaa@users.noreply.github.com>
2025-12-10 09:17:43 +0700

    commit

---
+++ TrainerCourse/TrainerCourse.Backend/DbMapper/IniProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using TrainerCourse.Backend.DTO;
+using TrainerCourse.Backend.Models;
+
+namespace TrainerCourse.Backend.DbMapper
+{
+    public class IniProfile : Profile
+    {
+        public IniProfile()
+        {
+            // Course Mapping
+            CreateMap<Course, CourseDTO>()
+                .ForMember(dest => dest.Trainer, opt => opt.MapFrom(src => src.Trainer))
+                .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl))
+                .ForMember(dest => dest.ImageFileName, opt => opt.MapFrom(src => src.ImageFileName));
+
+            CreateMap<CourseAddDTO, Course>();
+            CreateMap<Course, CourseAddDTO>();
+
+            // Instructor Mapping
+            CreateMap<Trainer, TrainerDTO>();
+            CreateMap<TrainerDTO, Trainer>();
+
+            CreateMap<Trainer, TrainerAddDTO>();
+            CreateMap<TrainerAddDTO, Trainer>();
+        }
+    }
+}
---
+++ TrainerCourse/TrainerCourse/TrainerCourse/Platforms/Android/MainApplication.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Runtime;
+
+[assembly: UsesPermission(Android.Manifest.Permission.Camera)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage)]
+
+namespace TrainerCourse
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
